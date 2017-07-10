1048b8a44905a03883c18e24f913e52a14736055
Antonia Cerda <antoniacerdag@gmail.com>
2017-07-10 17:11:43 -0400

    agregué un forEach en ambas funciones, para que muestre todos los ingredientes y todos los pasos de las recetas

--- js/script-recipe.js
+++ js/script-recipe.js
@@ -13,21 +13,27 @@ $(document).ready(function() {
 
 
 function showRecipe(recipes){
-	var titulo = $(recipesArray).attr("title");
-	var ingredientes = $(recipesArray).attr("ingredients");
-
-	$(".js-show-recipe").click(function(e) {
-		$(".list-ingredients").append("<li class='item-ingredient'>"+ ingredientes + "</li>")
-		$(".page").removeClass('make');
-	});
+	
+	recipes.forEach(function(r){
+		var titulo = $(r).attr("title");
+		var ingredientes = $(r).attr("ingredients");
+		$(".js-show-recipe").click(function() {
+			$(".list-ingredients").append("<li class='item-ingredient'>"+ ingredientes + "</li>")
+			$(".page").removeClass('make');
+		});
+	})
+	
 
 }
 
 function makeRecipe(recipes){
-	var make = $(recipesArray).attr("directions");
-	$(".js-show-make").click(function(event) {
-		$(".list-directions .item-direction").append(make);
-		$(".page").addClass('make');
-	});
+	recipes.forEach(function(r){
+		var make = $(r).attr("directions");
+		$(".js-show-make").click(function() {
+			$(".list-directions ").append("<li class='item-direction'>" + make + "</li>");
+			$(".page").addClass('make');
+		});
+	})
+	
 	
 }
